e9d3272450641edf037fcf10320aa1806c4d04b3
Iann <iconian4@gmail.com>
2025-05-14 23:28:57 +0300

    python

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        matches = []
+        sorted_word = sorted(self.word)
+
+        for candidates in candidates:
+            if(len(candidates) == len(self.word) and candidates.lower() != self.word):
+                if sorted(candidates.lower()) == sorted_word:
+                    matches.append(candidates)
+        return matches
